aba93d05206bc26087874b2e2f889e4de652b741
abhoyarGit <98224140+abhoyarGit@users.noreply.github.com>
2024-04-23 22:09:47 +0530

    Create Basic_class_inheritnce.py

---
+++ Inheritance/Basic_class_inheritnce.py
@@ -0,0 +1,66 @@
+class A:
+    def __init__(self):
+        print("Class A")
+class B(A):
+    def __init__(self):
+        print("Class B")
+class C(B):
+  def __init__(self):
+    print("Class C")
+obj_c = C()
+
+
+class A:
+    def __init__(self):
+        print("Class A")
+class B(A):
+    def __init__(self):
+        print("Class B")
+class C(B):
+    pass
+obj_c = C()
+
+
+
+class A:
+    def __init__(self):
+        print("Class A")
+class B(A):
+    def fun_b(self):
+        print("Class B")
+class C(B):
+    print("Class C")
+obj_c = C()
+
+
+
+class A:
+    def __init__(self):
+        print("Class A")
+class B(A):
+    def __init__(self):
+        print("Class B")
+class C(B):
+    print("Class C")
+obj_c = C()
+
+
+class A:
+    def __init__(self):
+        print("Class A")
+class B(A):
+        print("Class B")
+class C(B):
+  def __init__(self):
+    print("Class C")
+obj_c = C()
+
+
+
+class A:
+        print("Class A")
+class B(A):
+        print("Class B")
+class C(B):
+    print("Class C")
+obj_c = C()
